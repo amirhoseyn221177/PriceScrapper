34d08d8b188f53a8be5e6d0a0b5f62c6a2d182e1
ayung24 <ayung.024@gmail.com>
2021-06-02 21:36:19 -0700

    add header & footer

--- price_scrapper_front/src/App.js
+++ price_scrapper_front/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/Header/Header'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
+        <NavBar />
+        <Footer />
+      </div>
+  </BrowserRouter>
   );
 }
+
 
 export default App;
---
+++ price_scrapper_front/src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>Built by team merge-conflict</p>
+        </div>
+    )
+}
+
+export default Footer

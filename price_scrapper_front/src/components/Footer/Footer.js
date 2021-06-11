9ca8011e4b1fce623242f7239f3b1982b0f8e524
Amir Sayyar <54318268+amirhoseyn221177@users.noreply.github.com>
2021-06-11 12:06:32 -0700

    Amir redux (#16)
    * add header & footer
    
    * add material-ui search bar
    
    * redux
    
    * dads
    
    * fixed
    
    * fixed
    
    Co-authored-by: ayung24 <ayung.024@gmail.com>
    Co-authored-by: ghiamalireza <ghiamalireza@gmail.com>

--- price_scrapper_front/src/components/Footer/Footer.js
+++ price_scrapper_front/src/components/Footer/Footer.js
@@ -1,8 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import { Link } from 'react-router-dom'
-=======
->>>>>>> f48a9da3ce8fa7cac7574569fa2e144459a27a3c
 import './Footer.css'
 
 const Footer = () => {

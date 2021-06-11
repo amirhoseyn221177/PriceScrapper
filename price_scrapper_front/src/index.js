f48a9da3ce8fa7cac7574569fa2e144459a27a3c
ghiamalireza <66710877+ghiamalireza@users.noreply.github.com>
2021-06-11 11:22:27 -0700

    redux fix (#11)
    * redux fix
    
    * redux fix

--- price_scrapper_front/src/index.js
+++ price_scrapper_front/src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+import { createStore, compose, applyMiddleware, combineReducers } from "redux"
+import {Provider} from 'react-redux'
+import thunk  from 'redux-thunk'
+import reducer from './Reducers/rootReducer'
 
+const rootreducer=combineReducers({
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootreducer, composeEnhancers(applyMiddleware(thunk)))
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <Provider store={store}>
+      <App />
+  </Provider>,
+  document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

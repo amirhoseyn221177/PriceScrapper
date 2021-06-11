4f4299657e2908c3296fe5b8eec314fae67ad4e8
ghiamalireza <ghiamalireza@gmail.com>
2021-06-11 10:56:04 -0700

    redux

--- price_scrapper_front/src/index.js
+++ price_scrapper_front/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer'
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 

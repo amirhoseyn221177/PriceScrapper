e8c542855ad807095c41a7281ba305e26bbbd3ab
ayung24 <ayung.024@gmail.com>
2021-06-21 23:37:16 -0700

    add filter button with popup dialog for categories

--- price_scrapper_front/src/components/ProductCard/ProductCard.js
+++ price_scrapper_front/src/components/ProductCard/ProductCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 import { Button } from '@material-ui/core';
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import './ProductCard.css'
 
 const ProductCard = () => {
--- price_scrapper_front/src/components/Searchbar/Searchbar.js
+++ price_scrapper_front/src/components/Searchbar/Searchbar.js
@@ -1,14 +1,64 @@
 import SearchBar from "material-ui-search-bar";
-import React from 'react'
-import './Searchbar.css'
+import React, { useState } from 'react';
+import { Button, Dialog, FormControlLabel, IconButton, Checkbox, DialogTitle, DialogContent } from "@material-ui/core";
+import CloseIcon from '@material-ui/icons/Close';
+import './Searchbar.css';
 
 const Searchbar = () => {
+    const [open, setOpen] = useState(false);
+
+    const handleShow = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const categories = ["Clothing", "Shoes", "Computers", "Cars"]
+
     return (
-        <SearchBar className="searchBar"
+        <div className="searchDiv">
+            <SearchBar className="searchBar"
             //value={this.state.value}
             //onChange={(newValue) => this.setState({ value: newValue })}
             //onRequestSearch={() => doSomethingWith(this.state.value)}
-        />
+            />
+            <Button
+                className="filterButton"
+                variant="contained"
+                color="primary"
+                onClick={handleShow}
+            >
+                Filter
+            </Button>
+            <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+                <DialogTitle className="dialogTitle" disableTypography>
+                    <h2>Filter by category</h2>
+                    <IconButton onClick={handleClose}>
+                        <CloseIcon />
+                    </IconButton>
+                </DialogTitle>
+                <DialogContent dividers className="dialogContent">
+                    <div>
+                        {categories.map(
+                            (category, index) => {
+                                return (
+                                    <FormControlLabel
+                                        key={index}
+                                        control={
+                                            <Checkbox
+                                                name="checkedB"
+                                                color="primary"
+                                            />
+                                        }
+                                        label={category}
+                                    />
+                                )
+                            })}
+                    </div>
+                </DialogContent>
+            </Dialog>
+        </div>
     );
 }
 

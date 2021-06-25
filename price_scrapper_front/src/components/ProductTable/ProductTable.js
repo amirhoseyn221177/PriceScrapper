1827f8a4ba02324b6ccc7a53418f4f7c4a9f6853
satyakhanda <satyak.handa@gmail.com>
2021-06-24 20:20:00 -0700

    cards prototype working

--- price_scrapper_front/src/components/ProductTable/ProductTable.js
+++ price_scrapper_front/src/components/ProductTable/ProductTable.js
@@ -1,29 +1,100 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from "react";
 import { Table } from 'react-bootstrap';
 import './ProductTable.css';
 import ProductCard from '../ProductCard/ProductCard';
 import Searchbar from '../Searchbar/Searchbar';
-const ProductTable = () => {
+const ProductTable = (props) => {
+
+    var [ebayArray, setEbayArray] = useState([]);
+    var [stockXArray, setStockXArray] = useState([]);
+    var [amazonArray, setAmazonArray] = useState([]);
+
+    var [productInfoArray, setproductInfoArray] = useState([]);
+    var [productCardsJSX, setproductCardsJSX] = useState([]);
+
+
+
+    var setProductsArray = (ebayArrayFromAPI, stockXArrayFromAPI, amazonArrayFromAPI) => {
+        try {
+            setEbayArray(ebayArrayFromAPI);
+            setStockXArray(stockXArrayFromAPI);
+            setAmazonArray(amazonArrayFromAPI);
+            console.log("ebay array is: ", ebayArray)
+            console.log("ebay array length from setter", ebayArray.length )
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    var loadProductCards = () => {
+        console.log("has waited for previous")
+        let currProductsArray = [];
+        console.log("length", ebayArray.length)
+        for (let i = 0; i < ebayArray.length; i++) {
+            let title = ebayArray[i].title[0];
+            let vendor = "Ebay";
+            let price = ebayArray[i].sellingStatus[0].convertedCurrentPrice[0].value
+            let currency = ebayArray[i].sellingStatus[0].convertedCurrentPrice[0].convertedCurrentPrice
+  //          let image = ebayArray[i]?.pictureURLLarge[0] ?? "";
+
+            let ebayObject = {title, vendor, price, currency};
+            console.log("object", ebayObject)
+
+            currProductsArray.push(ebayObject)
+            console.log("getting populated: ", currProductsArray)
+        }
+        setproductInfoArray(currProductsArray) 
+    }
+
+    var createProductCards = () => {
+        let allCards = []
+        for (let i = 2; i < productInfoArray.length; i += 3) {
+            // <tr>
+            //     <td><ProductCard /></td>
+            //     <td><ProductCard /></td>
+            //     <td><ProductCard /></td>
+            // </tr>
+            allCards.push(
+                <tr>
+                    <td>
+                        <ProductCard 
+                            title={productInfoArray[i].title}
+                            vendor={productInfoArray[i].vendor}
+                            price={productInfoArray[i].price}
+                            currency={productInfoArray[i].currency}
+                        />
+                    </td>
+                    <td>
+                        <ProductCard 
+                            title={productInfoArray[i-1].title}
+                            vendor={productInfoArray[i-1].vendor}
+                            price={productInfoArray[i-1].price}
+                            currency={productInfoArray[i-1].currency}
+                        />
+                    </td>
+                    <td>
+                        <ProductCard 
+                            title={productInfoArray[i-2].title}
+                            vendor={productInfoArray[i-2].vendor}
+                            price={productInfoArray[i-2].price}
+                            currency={productInfoArray[i-2].currency}
+                        />
+                    </td>
+                </tr>
+            );
+        }
+        setproductCardsJSX(allCards)
+    }
+
+    useEffect(loadProductCards, [ebayArray]);
+    useEffect(createProductCards, [productInfoArray]);
+
     return (
         <Fragment>
-            <Searchbar />
+            <Searchbar setProductsArray={setProductsArray}/>
             <Table className="productTable">
                 <tbody>
-                    <tr>
-                        <td><ProductCard /></td>
-                        <td><ProductCard /></td>
-                        <td><ProductCard /></td>
-                    </tr>
-                    <tr>
-                        <td><ProductCard /></td>
-                        <td><ProductCard /></td>
-                        <td><ProductCard /></td>
-                    </tr>
-                    <tr>
-                        <td><ProductCard /></td>
-                        <td><ProductCard /></td>
-                        <td><ProductCard /></td>
-                    </tr>
+                    {productCardsJSX}
                 </tbody>
             </Table>
         </Fragment>
--- price_scrapper_front/src/components/Searchbar/Searchbar.js
+++ price_scrapper_front/src/components/Searchbar/Searchbar.js
@@ -1,15 +1,71 @@
 import SearchBar from "material-ui-search-bar";
-import React from 'react'
-import './Searchbar.css'
+import React, { Fragment, useEffect, useState } from "react";
+import "./Searchbar.css";
+import axios from "axios";
 
-const Searchbar = () => {
+
+class Searchbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchString: "",
+    };
+  }
+
+//   callAPI = (searchString) => {
+//     console.log(searchString);
+//     axios.post("http://localhost:8080/api/query/search", {searchString})
+//     .then(response => {
+//       console.log(response);
+//     })
+//     .catch(error => {
+//       console.log(error.response)
+//     });
+//     this.setState({ value: "" })
+//   };
+
+callAPI = async (searchString) => {
+    try {
+       var ebayResponse = await  axios.post("/api/ebay/search", {searchString});
+       const ebayJSON = ebayResponse.data
+       console.log(ebayJSON)
+       const ebayItemArr = ebayJSON.result[0].item
+    //    this.props.ebayItemArr = ebayItemArr
+
+       var stockxResponse = await  axios.post("/api/stockx/search", {searchString}); 
+       const stockxJSON = stockxResponse.data
+       const stockxItemArr = stockxJSON.result
+       console.log(stockxItemArr)
+
+    //    this.props.stockxItemArr = stockxItemArr
+
+       var amazonResponse = await  axios.post("/api/amazon/search", {searchString}); 
+       const amazonJSON = amazonResponse.data
+       const amazonItemArr = amazonJSON.result
+       console.log(amazonItemArr)
+
+    //    this.props.amazonItemArr = amazonItemArr
+
+
+        this.props.setProductsArray(ebayItemArr, stockxItemArr, amazonItemArr);
+
+       this.setState({ searchString: "" })
+
+    } catch(e) {
+        console.log(e.response.data.error.message)
+    }
+};
+
+  render() {
     return (
-        <SearchBar className="searchBar"
-            //value={this.state.value}
-            //onChange={(newValue) => this.setState({ value: newValue })}
-            //onRequestSearch={() => doSomethingWith(this.state.value)}
-        />
+      <SearchBar
+        id="Search-Bar"
+        value={this.state.value}
+        onChange={(newValue) => this.setState({ value: newValue })}
+        onRequestSearch={() => this.callAPI(this.state.value)}
+      />
     );
+  }
 }
 
-export default Searchbar
+export default Searchbar;

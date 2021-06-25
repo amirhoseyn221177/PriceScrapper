3143c04eecdafff222132b071bcba075392f5fa1
ayung24 <ayung.024@gmail.com>
2021-06-24 22:24:26 -0700

    add sort by buttons

--- price_scrapper_front/src/components/ProductTable/ProductTable.js
+++ price_scrapper_front/src/components/ProductTable/ProductTable.js
@@ -1,33 +1,90 @@
 import React from 'react';
 import { Table } from 'react-bootstrap';
 import './ProductTable.css';
-import { Button } from '@material-ui/core';
+import { List, ListItemText, ListItem, Menu, MenuItem } from '@material-ui/core';
 import ProductCard from '../ProductCard/ProductCard';
 
 const ProductTable = () => {
-    return (
-            <div className="tableDiv">
-            <Table className="productTable">
-                <tbody>
-                    <tr>
-                        <td><ProductCard /></td>
-                        <td><ProductCard /></td>
-                        <td><ProductCard /></td>
-                    </tr>
-                    <tr>
-                        <td><ProductCard /></td>
-                        <td><ProductCard /></td>
-                        <td><ProductCard /></td>
-                    </tr>
-                    <tr>
-                        <td><ProductCard /></td>
-                        <td><ProductCard /></td>
-                        <td><ProductCard /></td>
-                    </tr>
-                </tbody>
-            </Table>
-            </div>
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [selectedIndex, setSelectedIndex] = React.useState(1);
 
+    const handleClickListItem = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuItemClick = (event, index) => {
+        setSelectedIndex(index);
+        setAnchorEl(null);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const options = [
+        'Highest Rating',
+        'Lowest Rating',
+        'Highest Price',
+        'Lowest Price'
+    ];
+    return (
+        <div >
+            <div className="tableDiv">
+                <Table className="productTable">
+                    <thead>
+                        <tr>
+                            <th colSpan='3'>
+                                <List component="nav" aria-label="Device settings">
+                                    <ListItem
+                                        button
+                                        aria-haspopup="true"
+                                        aria-controls="lock-menu"
+                                        aria-label="Sort By:"
+                                        onClick={handleClickListItem}
+                                    >
+                                        <ListItemText primary={"Sort by: " + options[selectedIndex]} />
+                                    </ListItem>
+                                </List>
+                                <Menu
+                                    id="lock-menu"
+                                    anchorEl={anchorEl}
+                                    keepMounted
+                                    open={Boolean(anchorEl)}
+                                    onClose={handleClose}
+                                >
+                                    {options.map((option, index) => (
+                                        <MenuItem
+                                            key={option}
+                                            selected={index === selectedIndex}
+                                            onClick={(event) => handleMenuItemClick(event, index)}
+                                        >
+                                            {option}
+                                        </MenuItem>
+                                    ))}
+                                </Menu>
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><ProductCard /></td>
+                            <td><ProductCard /></td>
+                            <td><ProductCard /></td>
+                        </tr>
+                        <tr>
+                            <td><ProductCard /></td>
+                            <td><ProductCard /></td>
+                            <td><ProductCard /></td>
+                        </tr>
+                        <tr>
+                            <td><ProductCard /></td>
+                            <td><ProductCard /></td>
+                            <td><ProductCard /></td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
+        </div>
     );
 };
 

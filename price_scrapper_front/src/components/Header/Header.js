fc1126b1558ec5ccf9d47f69389b5e13065e074f
ayung24 <ayung.024@gmail.com>
2021-07-15 01:00:02 -0700

    add profile page

--- price_scrapper_front/src/components/Header/Header.js
+++ price_scrapper_front/src/components/Header/Header.js
@@ -10,6 +10,7 @@ const Header = () => {
                     <li><Link to="/home"> Home </Link></li>
                     <li><Link to="/login"> Login </Link></li>
                     <li><Link to="/register"> Register </Link></li>
+                    <li><Link to="/profile"> Profile </Link></li>
                 </ul>
             </nav>
         </div>
---
+++ price_scrapper_front/src/components/Profile/Profile.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { TextField, Button } from '@material-ui/core';
+import './Profile.css';
+
+const Profile = () => {
+
+    return (
+        <div><p>hi</p></div>
+    );
+};
+
+export default Profile;

ba71748aa79ba8b15077dddf8697b8757aa9ae7f
ayung24 <ayung.024@gmail.com>
2021-07-15 15:41:52 -0700

    add missing imports

--- price_scrapper_front/src/components/Profile/Profile.js
+++ price_scrapper_front/src/components/Profile/Profile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Profile.css';
 import axios from 'axios';
 import ProductCard from '../ProductCard/ProductCard';

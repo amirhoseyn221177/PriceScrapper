fb60deed1d5d2cc71b1834ead5d5aedc5a0e60cf
Satyak Handa <45611870+satyakhanda@users.noreply.github.com>
2021-07-15 22:40:19 -0700

    suggested items implemented (#42)

--- price_scrapper_front/src/components/ProductPage/ProductDetail.js
+++ price_scrapper_front/src/components/ProductPage/ProductDetail.js
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { Fragment, useEffect, useMemo, useState } from "react";
 import { Card  } from 'react-bootstrap'
 import { withRouter} from 'react-router';
 import { Button } from '@material-ui/core';
 import './ProductDetail.css'
 import SuggestedItems from '../SuggestedItems/SuggestedItems';
 import { connect } from 'react-redux';
+import { ChosenItem } from "../Actions/actions";
+
 
 const ProductDetail = (props) => {
+    console.log(props)
+    const [index, setIndex] = useState(props.location.state.index);
+    console.log(index)
+    var setProductIndex = (index)=> {
+        setIndex(index);
+    }
+    // var goToProductPage = () => {
+    //     let item = props.location.state[index];
+    //     // props.sendingItemArray(item);
+    //     props.history.push({
+    //         pathname: '/productdetail',
+    //         state: props.location.state
+    //     });
+    // };
 
     return (
         <div>
             <Card style={{ width: '40rem', height: '30rem', position: 'relative', left: '31.5%', marginTop: '5%'}}>
-                <Card.Img variant="top" src={props.item.image} width= '640' height= '480'/>
+                <Card.Img variant="top" src={props.location.state.productInfoArray[index].image} width= '640' height= '480'/>
                 <Card.Body className="cardBody">
                 </Card.Body>
             </Card>
             <p >
-                Name: {props.item.title}
+                Name: {props.location.state.productInfoArray[index].title}
             </p>
             <p>
-                Vendor: {props.item.vendor}
+                Vendor: {props.location.state.productInfoArray[index].vendor}
             </p>
             <p>
-                Price: {props.item.price} {props.item.currency}
+                Price: {props.location.state.productInfoArray[index].price} {props.location.state.productInfoArray[index].currency}
             </p>
             <p>
                 Rating: 
@@ -38,7 +54,9 @@ const ProductDetail = (props) => {
                     Buy product!
                 </Button>
             </a>
-           <SuggestedItems />
+           <SuggestedItems 
+                allItems = {props.location.state.productInfoArray} setProductIndex = {setProductIndex}
+           />
         </div>
     )
 }
@@ -46,8 +64,13 @@ const ProductDetail = (props) => {
 
 const mapToState= state=>{
     return{
-        item:state.item
+        item:state.item,
     }
 }
+// const mapToProps = dispatch => {
+//     return {
+//         sendingItemArray: (item) => dispatch(ChosenItem(item))
+//     };
+// };
 
 export default connect(mapToState,null) (withRouter(ProductDetail));

90eee6c1832736c2ba06143e78fa56288fd9ccd6
ayung24 <ayung.024@gmail.com>
2021-07-17 11:27:39 -0700

    remove auth buttons when logged in

--- price_scrapper_front/src/components/Header/Header.js
+++ price_scrapper_front/src/components/Header/Header.js
@@ -17,8 +17,6 @@ const Header = (props) => {
                         (
                             <ul>
                                 <li><Link to="/home"> Home </Link></li>
-                                <li><Link to="/login"> Login </Link></li>
-                                <li><Link to="/register"> Register </Link></li>
                                 <li><Link to="/profile"> Profile </Link></li>
                                 <li><Button onClick={logOut}>Logout</Button></li>
                             </ul>

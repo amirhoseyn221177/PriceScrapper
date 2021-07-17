136a4834d47c10cbad0b6796d92b8c132082d59d
ayung24 <ayung.024@gmail.com>
2021-07-16 19:47:21 -0700

    add logout function

--- price_scrapper_front/src/components/Header/Header.js
+++ price_scrapper_front/src/components/Header/Header.js
@@ -1,20 +1,41 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from '@material-ui/core';
+import { withRouter } from 'react-router';
 import './Header.css'
 
-const Header = () => {
+const Header = (props) => {
+    function logOut() {
+        localStorage.removeItem("token")
+        props.history.push("/home")
+    }
     return (
         <div className="navbar">
             <nav className="nav">
-                <ul>
-                    <li><Link to="/home"> Home </Link></li>
-                    <li><Link to="/login"> Login </Link></li>
-                    <li><Link to="/register"> Register </Link></li>
-                    <li><Link to="/profile"> Profile </Link></li>
-                </ul>
+                {
+                    localStorage.getItem("token") ?
+                        (
+                            <ul>
+                                <li><Link to="/home"> Home </Link></li>
+                                <li><Link to="/login"> Login </Link></li>
+                                <li><Link to="/register"> Register </Link></li>
+                                <li><Link to="/profile"> Profile </Link></li>
+                                <li><Button onClick={logOut}>Logout</Button></li>
+                            </ul>
+
+                        )
+                        :
+                        (
+                            <ul>
+                                <li><Link to="/home"> Home </Link></li>
+                                <li><Link to="/login"> Login </Link></li>
+                                <li><Link to="/register"> Register </Link></li>
+                            </ul>
+                        )
+                }
             </nav>
         </div>
     )
 }
 
-export default Header
+export default withRouter(Header)
